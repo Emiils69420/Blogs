ac6aed09cfdd615a9f0f344d965dd107e99aad8b
Emiils69420 <126755996+Emiils69420@users.noreply.github.com>
2024-02-27 13:27:44 +0200

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE blog_darzins;
+-- USE DATABASE blog_darzins;
+-- CREATE TABLE posts (
+/*
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+title VARCHAR(255) NOT NULL
+);
+*/
+/*
+INSERT INTO posts(title) VALUES
+("My First Blog Post"),("My Second Blog Post");
+*/
+SELECT * FROM posts;
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require "functions.php";
+
+echo "high";
+
+$connection_string = "mysql:host=localhost;dbname=blog_darzins;user=root;password=;charset=utf8mb4";
+$pdo = new PDO($connection_string);
+
+$query = $pdo->prepare("SELECT * FROM posts");
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<ul>";
+foreach($posts as $post){
+    
+    echo "<li>" .$post["title"] . "</li>";
+}
+echo"</ul>";
